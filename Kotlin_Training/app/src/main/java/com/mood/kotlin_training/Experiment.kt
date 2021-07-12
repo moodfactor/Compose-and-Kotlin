0f56beb7418c804fcf0a975bbf87c8ae043d8c9b
Ahmed Mansour <moodfactor@hotmail.com>
2021-07-12 17:10:30 +0200

    created a powerful get clone function

--- Kotlin_Training/app/src/main/java/com/mood/kotlin_training/Experiment.kt
+++ Kotlin_Training/app/src/main/java/com/mood/kotlin_training/Experiment.kt
@@ -2,10 +2,18 @@ package com.mood.kotlin_training
 
 fun main(){
     val listOfNames = listOf<String>("Ahmed", "Moayad", "Aya")
-    val getItInvoke = listOfNames.getFirstElement {l -> l[1]  }
-    println(getItInvoke)
+    val listOfNumbers = listOf<Int>(1, 2, 3)
+//    val getItInvoke = listOfNames.getFirstElement{list -> list[1] }
+    val operation = listOfNumbers.getAnyElement { i -> i[1] }
+    println(operation)
 }
 
-fun List<String>.getFirstElement(getIt: (List<String>) -> String): String{
+// return index of any type of lists just like get collection function
+fun <T> List<T>.getAnyElement(getIt: (List<T>) -> T): T {
     return getIt(this)
+//    return getFirstElement { l -> l[1] }
+}
+
+fun addTwoNumbers(add: (a: Int,b: Int) -> Int): Int{
+    return addTwoNumbers { a, b -> a+b }
 }
