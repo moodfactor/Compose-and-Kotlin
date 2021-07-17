5644f0aedafa1984b8ee9a88d71dc88d45432c69
Ahmed Mansour <moodfactor@hotmail.com>
2021-07-17 23:14:52 +0200

    added spinWords fun to codeWars.com test

--- Kotlin_Training/app/src/main/java/com/mood/kotlin_training/Experiment.kt
+++ Kotlin_Training/app/src/main/java/com/mood/kotlin_training/Experiment.kt
@@ -1,14 +1,100 @@
 package com.mood.kotlin_training
 
 fun main() {
-    val listOfNames = listOf<String>("Ahmed", "Moayad", "Aya")
-    val listOfNumbers = listOf<Int>(1, 2, 3)
-//    val getItInvoke = listOfNames.getFirstElement{list -> list[1] }
-    val operation = listOfNumbers.getAnyElement()
-    println(operation)
-    val listOfNumber = listOf(1, 5, 87, 45, 8, 8)
-    println(twoOldestAges(listOfNumber))
+//    val listOfNames = listOf<String>("Ahmed", "Moayad", "Aya")
+//    val listOfNumbers = listOf<Int>(1, 2, 3)
+////    val getItInvoke = listOfNames.getFirstElement{list -> list[1] }
+//    val operation = listOfNumbers.getAnyElement()
+//    println(operation)
+//    val listOfNumber = listOf(1, 5, 87, 45, 8, 8)
+//    println(twoOldestAges(listOfNumber))
+    val words = "This is another test"
+//    val word = "Another"
+//    println(word.reversed())
+//    println(words.split("\\s+".toRegex(),0).filter { s -> s.length > 5 }.last().reversed())
+//    println(words.split(" ").apply {
+//        var ss = StringBuilder()
+//        for (s in this){
+//            if (s.length > 5){
+//              ss.append(s)
+//            }
+//            return@apply
+//        }
+//    }.toString())
+//    println(words.split(" ").filter { s -> s.length >5 }.last().reversed())
+//    println(words.replace(words,transform= () ))
+    println(words.split(" ").toMutableList().toString())
+    println(spinWords(words))
 }
+
+//public String spinWords(String sentence) {
+//    String[] arr = sentence.split(" ");
+//    for(int i=0;i<arr.length;i++){
+//        if(arr[i].length() >= 5)
+//            arr[i] = new StringBuffer(arr[i]).reverse().toString();
+//    }
+//    return String.join(" ",arr);
+//}
+//}
+
+fun spinWords(sentence: String): String{
+    var separate = sentence.split(" ").toMutableList()
+    for (n in separate){
+        if (n.length >5){
+            val rev = n.reversed().toString()
+            separate.add(rev)
+            break
+        }else{
+            separate.add(n)
+            break
+        }
+    }
+    return separate.toList().toString()
+
+}
+
+
+//fun spinWords(sentence: String): String {
+//    var backword = "";
+//    var separate = sentence.split(" ");
+//
+//    for (n in separate) {
+//        if (n.length >= 5) {
+//            backword += n.split(" ").reversed().joinToString(",")
+//        } else {
+//            backword += n
+//        }
+//    }
+//    return backword
+//}
+//    val final = ""
+//    val seperate = sentence.split("\\s+")
+//    val stringBuilder = StringBuilder()
+//   for (s in seperate){
+//       when(s.length){
+//           2 -> stringBuilder.append(s)
+//           7 -> s.reversed().also { stringBuilder.append(s) }
+//           else -> println("error")
+//       }
+//       return stringBuilder.toString()
+//   }
+//    return stringBuilder.toString()
+
+
+//fun spinWords(sentence: String): String {
+//    val final = ""
+//    val separate = sentence.split("\\s+")
+//    var stringBuilder = StringBuilder()
+//    for (n in separate){
+//        if (n.length <5) {
+//            stringBuilder.append(n)
+//        }else if (n.length > 5){
+//            val ss = n.reversed()
+//            stringBuilder.append(ss)
+//        }
+//    }
+//    return stringBuilder.toString()
+//}
 
 fun twoOldestAges(ages: List<Int>): List<Int> {
     return ages.sortedDescending().take(2).sorted()
